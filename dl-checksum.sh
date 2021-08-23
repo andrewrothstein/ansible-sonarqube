e049e516d8c7208b16e44fca8efaa7f1cacdfeaa
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-08-22 21:01:46 -0400

    sonarqube:9.0.1.46107

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+set -e
 DIR=~/Downloads
 MIRROR=https://binaries.sonarsource.com/Distribution/sonarqube
 
@@ -7,10 +7,14 @@ dl_ver() {
     ver=$1
     local file=sonarqube-$ver.zip
     local lfile=$DIR/$file
-    local url=$MIRROR/sonarqube-$ver.zip.sha
+    local url=$MIRROR/sonarqube-$ver.zip
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
 
     printf "  # %s\n" $url
-    printf "  '%s': sha1:%s\n" $ver $(curl -sSL $url)
+    printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
 }
 
-dl_ver ${1:-8.1.0.31237}
+dl_ver ${1:-9.0.1.46107}
